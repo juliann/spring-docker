65fd724de1c27896260b5d6b5dc6f68c56327adf
Julian Nadarzy <julian.nadarzy@gmail.com>
2021-09-10 11:35:53 +0200

    added docker-compose files, yaml basics

--- src/main/java/guru/springframework/controllers/IndexController.java
+++ src/main/java/guru/springframework/controllers/IndexController.java
@@ -1,25 +1,40 @@
 package guru.springframework.controllers;
 
+import guru.springframework.model.events.PageViewEvent;
+import guru.springframework.pageview.PageViewService;
 import guru.springframework.services.ProductService;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.Date;
+import java.util.UUID;
 
 
 @Controller
 public class IndexController {
 
     private final ProductService productService;
+    private final PageViewService pageViewService;
 
 
-    public IndexController(ProductService productService) {
+    public IndexController(ProductService productService, PageViewService pageViewService) {
         this.productService = productService;
+        this.pageViewService = pageViewService;
     }
 
     @RequestMapping({"/", "index"})
     public String getIndex(Model model) {
 
         model.addAttribute("products", productService.listProducts());
+
+        PageViewEvent pageViewEvent = new PageViewEvent();
+        pageViewEvent.setPageUrl("springframework.guru/");
+        pageViewEvent.setPageViewDate(new Date());
+        pageViewEvent.setCorrelationId(UUID.randomUUID().toString());
+
+        pageViewService.sendPageViewEvent(pageViewEvent);
+
 
         return "index";
     }
